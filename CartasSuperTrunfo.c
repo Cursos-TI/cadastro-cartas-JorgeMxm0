87de79d38b3625d923131a95ddea530460b39996
JorgeMxm0 <jorgemesquita039@gmail.com>
2025-09-24 01:58:16 +0000

    cartas jorge

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -2,46 +2,64 @@
 
 int main (){
 
- // VARIAVEIS PARA A CARTA 1 (RORAIMA)
+    // VARIAVEIS PARA A CARTA 1 (RORAIMA)
+    int populacao_roraima;
+    double pib_roraima;
+    double area_roraima;
+    int pontos_turisticos_roraima;
 
- int populaçao_roraima;
- double pib_roriama;
- double area_roriama;
- int ponto_turisticos_roraima;
+    // VARIAVEIS PARA A CARTA 2 (MANAUS)
+    int populacao_manaus;
+    double pib_manaus;
+    double area_manaus;
+    int pontos_turisticos_manaus;
 
- // VARIAVEIS PARA A CARTA 2 (MANAUS)
+    // DADOS PARA A CARTA DE RORAIMA
+    printf("--- Carta de Roraima ---\n");
+    printf("Digite a populacao de Roraima: ");
+    scanf("%d", &populacao_roraima);
 
- int populacao_manaus;
- double pib_manaus;
- double area_manaus;
- int ponto_turisto_manaus;
+    printf("Digite o PIB de Roraima: ");
+    scanf("%lf", &pib_roraima);
 
- // DADOS PARA A CARTA DE RORAIMA 1
- 
- printf(" --- Carta de Roraima ---\n");
- printf(" Digite a populacao de Roraima: \n");
- scanf(" %xd, &populaçao_roraima \n");
- printf("Digite o PIB de Roraima: \n");
- scanf("%lf, &pib_roriama \n");
- printf("Digite a area de Roraima: \n");
- scanf("%lf, &area_roriama \n");
- printf("Digite os pontos turisticos de Roraima: \n");
- scanf(" %xd, &ponto_turisticos_roraima \n");
- printf("\n");
+    printf("Digite a area de Roraima: ");
+    scanf("%lf", &area_roraima);
 
- // DADOS PARA A CARTA DE MANAUS 2
+    printf("Digite os pontos turisticos de Roraima: ");
+    scanf("%d", &pontos_turisticos_roraima);
 
- printf(" --- Carta de Manaus ---\n");
- printf(" Digite a populacao de Manaus: \n");
- scanf("%xd" , &populacao_manaus);
- printf("Digite a PIB de Manaus: \n");
- scanf("%lf , %pib_manaus \n");
- printf("Digite a area de Manaus: \n");
- scanf("%lf , &area_manaus \n");
- printf("Digite ponto turistico de Manaus \n");
- scanf("%xd , &ponto_turisticos_roraima \n ");
- printf("\n");
+    printf("\n");
 
-return 0;
+    // DADOS PARA A CARTA DE MANAUS
+    printf("--- Carta de Manaus ---\n");
+    printf("Digite a populacao de Manaus: ");
+    scanf("%d", &populacao_manaus);
 
+    printf("Digite o PIB de Manaus: ");
+    scanf("%lf", &pib_manaus);
+
+    printf("Digite a area de Manaus: ");
+    scanf("%lf", &area_manaus);
+
+    printf("Digite os pontos turisticos de Manaus: ");
+    scanf("%d", &pontos_turisticos_manaus);
+
+    printf("\n");
+
+    // EXIBINDO AS CARTAS
+    printf("===== CARTAS CADASTRADAS =====\n");
+
+    printf("\nCarta 1 - Roraima\n");
+    printf("Populacao: %d\n", populacao_roraima);
+    printf("PIB: %.2lf\n", pib_roraima);
+    printf("Area: %.2lf km²\n", area_roraima);
+    printf("Pontos turisticos: %d\n", pontos_turisticos_roraima);
+
+    printf("\nCarta 2 - Manaus\n");
+    printf("Populacao: %d\n", populacao_manaus);
+    printf("PIB: %.2lf\n", pib_manaus);
+    printf("Area: %.2lf km²\n", area_manaus);
+    printf("Pontos turisticos: %d\n", pontos_turisticos_manaus);
+
+    return 0;
 }
